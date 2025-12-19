299ad3904b320af516c09bc3ec4d0f96474fba57
manahylkhan <134911648+manahylkhan@users.noreply.github.com>
2025-12-20 02:25:05 +0500

    Add IAM role and policy for secure app

---
+++ iac/terraform/iam.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "app_role" {
+  name = "secure-app-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "app_policy" {
+  role = aws_iam_role.app_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["logs:CreateLogStream", "logs:PutLogEvents"]
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_instance_profile" "app_profile" {
+  role = aws_iam_role.app_role.name
+}
